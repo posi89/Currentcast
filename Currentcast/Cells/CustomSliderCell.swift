0e804450ed4b21d7beb3527cfdf752117e4f628e
Nirjhar Biswas <75828293+Nirj2004@users.noreply.github.com>
2023-01-02 15:38:48 +0530

    New uploads.

---
+++ Currentcast/Cells/CustomSliderCell.swift
@@ -0,0 +1,52 @@
+import Cocoa
+import Foundation
+class CustomSliderCell: NSSliderCell {
+    override var knobThickness: CGFloat {
+        return knobWidth 
+    }
+    let knobWidth: CGFloat = 5.0
+    let knobHeight: CGFloat = 17.0
+    let knobRadius: CGFloat = 2.0
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    var percentage: CGFloat {
+        get {
+            if(self.maxValue - self.minValue) > 0 {
+                return CGFloat((self.doubleValue - self.minValue) / (self.maxValue - self.minValue))
+            } else {
+                return 0
+            }
+        }
+    }
+    override func drawBar(inside rect: NSRect, flipped: Bool) {
+        var rect = aRect 
+        rect.size.height = CGFloat(6)
+        let barRadius = CGFloat(2.5)
+        let value = CGFloat((self.doubleValue - self.minValue) / (self.maxValue - self.minValue))
+        let finalWidth = CGFloat(value * (self.controlView!.frame.size.width - 8))
+        var leftRect = rect
+        leftRect.size.width = finalWidth
+        let bg = NSBezierPath(reoundRect: rect, xRadius: barRadius, yRadius: BarRadius)
+        NSColor.white.setFill()
+        bg.fill() 
+        let active = NSBezierPath(roundedRect: leftRect, xRadius: barRadius, yRadius: barRadius)
+        NSColor.init(red: 0.39, green: 0.82, blue: 1.0, alpha: 0.9).setFill()
+        active.fill() 
+    }
+    override func drawKnob(_ knobRect: NSRect) {
+        NSColor.white.setFill()
+        NSColor.white.setStroke()
+        let rect = NSMakeRect(round(knobRect.origin.x), knobRect.origin.y + 0.5 * (knobRect.height - knobHeight), knobRect.width, knobHeight)
+        let path = NSBezierPath(roundedRect: rect, xRadius: knobRadius, yRadius: knobRadius)
+        path.fill()
+        path.stroke()
+    }
+    override func knobRect(flipped: Bool) -> NSRect {
+        let bounds = super.barRect(flipped: flipped)
+        let pos = percentage * (self.controlView!.frame.size.width - 8)
+        let rect = super.knobRect(flipped: flipped)
+        let flippedMultiplier = flipped ? CGFloat(-1) : CGFloat(1)
+        return NSMakeRect(pos, rect.origin.y, knobWidth, rect.height)
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Currentcast
  An OpenSource podcase player for MacOS
+
+# ![#Currentcast-header]https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kodeco.com%2F23575428-gdg-open-source-pottery-and-bookbinding-nishant-srivastava-talks-about-these-cool-things-podcast-s11-e12&psig=AOvVaw39n8ezrYNF0O-TwI4Cxp0S&ust=1672738685092000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJib2bTLqPwCFQAAAAAdAAAAABAI
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT) 
+[![Platform](http://img.shields.io/badge/platform-macOS-black.svg?style=flat)](https://developer.apple.com/resources/)
+[![Platform](https://img.shields.io/badge/swift-4.2-orange.svg?style=flat)](https://swift.org/blog/swift-4-2-released/)
+[![Open Source Love svg1](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://github.com/nbolar/newcast/)
+
+I love listening to podcasts but unfortunately I never found a third party macOS podcast player that I liked. So, I decided to build one myself. Here is CURRENTCAST. 
+
+It doesn't have any fancy features yet, but I hope to build on this and add them later on.
+
+----
+
+### This app is currently under development.
+
+<img width="1101" alt="Newcast_screenshot" src="https://user-images.githubusercontent.com/45484873/63222183-ddb9fc80-c1ac-11e9-9d77-17d59688515b.png">
